56b868e7f528664cd3cf65737160fb19f4197fca
Lama Youzghatli <lama@voila.digital>
2023-06-10 08:02:14 +0300

    AppServiceProvider - Change default public path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,35 +24,39 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
         view()->composer('*', function ($view) {
-            $lang = config('app.locale');
-            $social_media = SocialMedia::where('active', 1)->orderBy('sorting')->get();
-            $settings = DB::table('cms_settings')->whereIn('group_setting', ['Application Setting', 'SEO Setting'])->pluck('content', 'name');
 
-            $company_information = CompanyInformation::where('active', 1)->first();
+            if (Route::current()) {
+                $lang = config('app.locale');
+                $social_media = SocialMedia::where('active', 1)->orderBy('sorting')->get();
+                $settings = DB::table('cms_settings')->whereIn('group_setting', ['Application Setting', 'SEO Setting'])->pluck('content', 'name');
 
-            if (Route::current()->uri == "/") {
-                $seo = Seo::where("page", "home")->where('language', $lang)->first();
-            } else if (strpos(Route::current()->uri, "{title}") === false) {
-                $seo = Seo::where("page", Route::current()->uri)->whereNull("page_id")->where('language', $lang)->first();
-            } else if (Route::current()->parameters) {
-                $temp = explode("/{title}", Route::current()->uri)[0];
-                $db_table = '';
-                if ($temp == 'blogs') {
-                    $db_table = 'articles';
-                } else {
-                    $db_table = $temp;
+                $company_information = CompanyInformation::where('active', 1)->first();
+
+                if (Route::current()->uri == "/") {
+                    $seo = Seo::where("page", "home")->where('language', $lang)->first();
+                } else if (strpos(Route::current()->uri, "{title}") === false) {
+                    $seo = Seo::where("page", Route::current()->uri)->whereNull("page_id")->where('language', $lang)->first();
+                } else if (Route::current()->parameters) {
+                    $temp = explode("/{title}", Route::current()->uri)[0];
+                    $db_table = '';
+                    if ($temp == 'blogs') {
+                        $db_table = 'articles';
+                    } else {
+                        $db_table = $temp;
+                    }
+                    $modul_id = DB::table($db_table)->where('slug', Route::current()->parameters["title"])->whereNull('deleted_at')->first()->id;
+                    $seo = Seo::where("page", $temp)->where("page_id", $modul_id)->where('language', $lang)->first();
                 }
-                $modul_id = DB::table($db_table)->where('slug', Route::current()->parameters["title"])->whereNull('deleted_at')->first()->id;
-                $seo = Seo::where("page", $temp)->where("page_id", $modul_id)->where('language', $lang)->first();
+
+                //if page don't has seo show site SEO
+                if (!$seo) {
+                    $seo = $seo = Seo::where("page", "home")->where('language', $lang)->first();
+                }
+                $view->with('seo', $seo);
+                $view->with('settings', $settings);
+                $view->with('social_media', $social_media);
+                $view->with('company_information', $company_information);
             }
-            //if page don't has seo show site SEO
-            if (!$seo) {
-                $seo = $seo = Seo::where("page", "home")->where('language', $lang)->first();
-            }
-            $view->with('seo', $seo);
-            $view->with('settings', $settings);
-            $view->with('social_media', $social_media);
-            $view->with('company_information', $company_information);
         });
 
         // View::share(['social_media' => $social_media, 'company_information' => $company_information]);
@@ -66,5 +70,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        $this->app->bind('path.public', function () {
+            return realpath(base_path() . '/../');
+        });
     }
 }
