2e352cb270bfc2e8d217961f604d7f0010b1666e
Lama Youzghatli <lama@voila.digital>
2023-06-14 12:45:37 +0300

    Add Force HTTPS middleware

--- storage/main/cloudsellpos_system/site/app/Http/Kernel.php
+++ storage/main/cloudsellpos_system/site/app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\Localization::class,
-
+            \App\Http\Middleware\ForceHttps::class
         ],
 
         'api' => [
@@ -67,6 +67,8 @@ class Kernel extends HttpKernel
         'localize' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
 		'localizationRedirect' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
 		'localeSessionRedirect' => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
-        'localeViewPath' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class
+        'localeViewPath' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
+
+        'force-https' => \App\Http\Middleware\ForceHttps::class,
     ];
 }
